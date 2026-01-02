fd781e50e6ef1aca7dd198106a9f2428b80b2265
anonymid <60818452+anonymid@users.noreply.github.com>
2026-01-02 21:46:34 +0700

    off

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time
 import random
-
+asd
 # --- CONFIG ---
 st.set_page_config(page_title="Penelitian Psikologi", layout="centered")
 GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxwN-PHPecqTdSZDyGiQyKAtfYNcLtuMeqPi8nGJ3gKlmFl3aCInGN0K_SlxmCZffKmXQ/exec"
